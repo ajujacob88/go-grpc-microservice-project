1fd919ccac225e943c1d80ac78a22a38dcba8338
ajujacob <ajujacob88@gmail.com>
2023-08-01 20:56:03 +0530

    auth service completed

--- go-grpc-api-gateway/cmd/main.go
+++ go-grpc-api-gateway/cmd/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+
+	"github.com/ajujacob88/go-grpc-microservice-project/go-grpc-api-gateway/pkg/auth"
+	"github.com/ajujacob88/go-grpc-microservice-project/go-grpc-api-gateway/pkg/config"
+	"github.com/ajujacob88/go-grpc-microservice-project/go-grpc-api-gateway/pkg/order"
+	"github.com/ajujacob88/go-grpc-microservice-project/go-grpc-api-gateway/pkg/product"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+
+	r := gin.Default()
+
+	authSvc := *auth.RegisterRoutes(r, &c)
+	product.RegisterRoutes(r, &c, &authSvc)
+	order.RegisterRoutes(r, &c, &authSvc)
+
+	r.Run(c.Port)
+}
