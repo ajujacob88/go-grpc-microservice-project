9ea2b130e354a231d0f4a8196824991ba7918b5d
ajujacob <ajujacob88@gmail.com>
2023-08-02 10:03:03 +0530

    product service completed

---
+++ go-grpc-product-svc/pkg/db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"log"
+
+	"github.com/ajujacob88/go-grpc-microservice-project/go-grpc-product-svc/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(url string) Handler {
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.Product{})
+	db.AutoMigrate(&models.StockDecreaseLog{})
+
+	return Handler{db}
+}
